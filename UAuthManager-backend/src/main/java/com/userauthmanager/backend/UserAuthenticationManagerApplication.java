07a72fc9b98aed77d3213c6fa600a5575640b2ad
Oladushek <gyrf921@gmail.com>
2023-10-02 16:54:17 +0300

    init commit for backend

---
+++ UAuthManager-backend/src/main/java/com/userauthmanager/backend/UserAuthenticationManagerApplication.java
@@ -0,0 +1,13 @@
+package com.userauthmanager.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAuthenticationManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthenticationManagerApplication.class, args);
+	}
+
+}
